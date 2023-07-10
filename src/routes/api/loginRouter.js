ca000138d5fc988213c8ae71b667e6a8df9df9d8
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-07-10 18:31:21 +0530

    Update loginRouter.js

--- src/routes/api/loginRouter.js
+++ src/routes/api/loginRouter.js
@@ -14,7 +14,7 @@ const transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: 'arun.maitexa@outlook.com',
-    password: 'Password@1007'
+    pass: 'Password@1007'
   }
 });
 
